720ed2086659e3022c5e491e5184869367ba10e2
Javier Cortejoso <javi@clabs.co>
2023-07-30 16:55:16 +0200

    Updated string

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours!";
+const hello = () => "Hello World from the Office Hours in the terminal!";
 console.log(hello());
 exports.hello = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours!");
+    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
   });
 });
