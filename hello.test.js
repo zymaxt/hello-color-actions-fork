66663bee832a7ed3670104268db11fce997ffd52
Javier Cortejoso <javier.cortejoso@gmail.com>
2023-07-30 14:51:20 +0000

    Arreglando los tests

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Spain");
+    expect(hello.hello()).toEqual("Hello World from the Office Hours!");
   });
 });
